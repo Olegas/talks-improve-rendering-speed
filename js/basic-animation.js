8491ebbf2ce31f8c424bdb1f132de84c01a5355f
Oleg Elifantiev <oleg@elifantiev.ru>
2015-05-27 14:55:11 +0300

    ОБернул код анимации в модули чтобы разные варианты друг с другом не пересекались

--- js/basic-animation.js
+++ js/basic-animation.js
@@ -1,31 +1,35 @@
-function step() {
-   var col = document.querySelectorAll('.snowflake:not(.steady)');
-   for (var i = 0, l = col.length; i < l; i++) {
-      var item = col[i];
-      item.style.top = item.getBoundingClientRect().top + item.stepY + 'px';
-      item.style.left = item.getBoundingClientRect().left + Math.sin(item.x += item.stepX) + 'px'
-   }
-}
+var basicAnimation = (function() {
 
-function check() {
-   var col = document.querySelectorAll('.snowflake:not(.steady)');
-   for (var i = 0, l = col.length; i < l; i++) {
-      var item = col[i];
-      var rect = item.getBoundingClientRect();
-      if (rect.bottom > window.innerHeight) {
-         item.classList.add('steady');
-         addToScene(generateSnowflake());
-         setTimeout(remove.bind(null, item), 1000);
+   function step() {
+      var col = document.querySelectorAll('.snowflake:not(.steady)');
+      for (var i = 0, l = col.length; i < l; i++) {
+         var item = col[i];
+         item.style.top = item.getBoundingClientRect().top + item.stepY + 'px';
+         item.style.left = item.getBoundingClientRect().left + Math.sin(item.x += item.stepX) + 'px'
       }
    }
-}
 
-function remove(item) {
-   item.parentNode.removeChild(item);
-}
+   function check() {
+      var col = document.querySelectorAll('.snowflake:not(.steady)');
+      for (var i = 0, l = col.length; i < l; i++) {
+         var item = col[i];
+         var rect = item.getBoundingClientRect();
+         if (rect.bottom > window.innerHeight) {
+            item.classList.add('steady');
+            addToScene(generateSnowflake());
+            setTimeout(remove.bind(null, item), 1000);
+         }
+      }
+   }
 
-function basicAnimation() {
-   step();
-   check();
-   setTimeout(basicAnimation, 0);
-}
+   function remove(item) {
+      item.parentNode.removeChild(item);
+   }
+
+   return function basicAnimationInner() {
+      step();
+      check();
+      setTimeout(basicAnimation, 0);
+   };
+
+})();
